7ea4ed077e9354268b2312eef41308147aed8e5d
Mark <tka4mark@gmail.com>
2023-11-15 16:22:59 +0200

    Update murkup

--- src/index.js
+++ src/index.js
@@ -1,32 +1,88 @@
-import SimpleLightbox from "simplelightbox";
 import Notiflix from 'notiflix';
-import { resetPage, searchImages } from './usersApi.js';
-import { getTotalHits } from './usersApi.js';
+import { resetPage, searchImages, perPage, currentPage } from './usersApi.js';
 
 const refs = {
-    form: document.querySelector('.search-form'),
-    searchBtn: document.querySelector('.header_button'),
-    divGallery: document.querySelector('.gallery'),
-    loadMoreBtn: document.querySelector('.load-more')
+    formEl: document.querySelector('.search-form'),
+    formBtn: document.querySelector('.btn'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
 };
-
 
 const hiddenClass = 'hidden';
 
 function showLoadMoreButton() {
     refs.loadMoreBtn.classList.remove(hiddenClass);
-};
-
-function hideLoadMoreButton() {
-    refs.loadMoreBtn.classList.add(hiddenClass);
-};
-
-refs.form.addEventListener('submit', onFormSubmit)
-
-async function onFormSubmit(e) {
-    e.preventDefault();
-    console.log('submit');
 }
 
+export function hideLoadMoreButton() {
+    refs.loadMoreBtn.classList.add(hiddenClass);
+}
 
+async function onFormSubmit(event) {
+    event.preventDefault();
+    const query = event.target.elements.searchQuery.value;
+    if (!query || /^\s*$/.test(query)) {
+        Notiflix.Notify.warning('Please enter a valid search query.');
+        return;
+    }
+    try {
+        resetPage();
+        const { hits } = await searchImages(query);
+        if (hits.length === 0) {
+            Notiflix.Notify.warning(
+                'Sorry, there are no images matching your search query. Please try again.'
+            );
+            hideLoadMoreButton();
+            return;
+        }
+        refs.gallery.innerHTML = '';
+        renderImages(hits);
+        showLoadMoreButton();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+async function onClickLoad() {
+    try {
+        const query = refs.formEl.elements.searchQuery.value;
+        const { hits, totalHits } = await searchImages(query);
+        if (currentPage * perPage >= totalHits) {
+            Notiflix.Notify.warning(
+                "We're sorry, but you've reached the end of search results."
+            );
+            hideLoadMoreButton();
+        }
+        renderImages(hits);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onClickLoad);
+
+function renderTemplate({ webformatURL, likes, views, comments, downloads }) {
+    return `<div class="photo-card">
+    <img class="photo" src="${webformatURL}" alt="No description" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div>`;
+}
+
+function renderImages(array) {
+    const markup = array.map(renderTemplate).join('');
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
--- src/usersApi.js
+++ src/usersApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const apiKey = '40663790-7e95e5f30e8cc12ce8787ce9f';
 const apiUrl = 'https://pixabay.com/api/';
-const perPage = 40;
-let currentPage = 1;
+export const perPage = 40;
+export let currentPage = 1;
 
 export async function searchImages(query) {
     const params = {
@@ -17,13 +17,13 @@ export async function searchImages(query) {
     };
 
     try {
-    const response = await axios.get(apiUrl, { params });
-    const images = response.data.hits;
-    currentPage += 1;
-    return images
-} catch (error) {
-    console.log(error)
-}
+        const response = await axios.get(apiUrl, { params });
+        const { hits, totalHits } = response.data;
+        currentPage += 1;
+        return { hits, totalHits };
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function resetPage() {
