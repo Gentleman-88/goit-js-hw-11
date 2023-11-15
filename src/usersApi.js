b97118b8caad34054660b87955c09f627100e70b
Mark <tka4mark@gmail.com>
2023-11-15 12:25:42 +0200

    Update template

--- src/usersApi.js
+++ src/usersApi.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
 
 const apiKey = '40663790-7e95e5f30e8cc12ce8787ce9f';
 const apiUrl = 'https://pixabay.com/api/';
-const perPage = 20;
+const perPage = 40;
 let currentPage = 1;
-let totalHits = 0;
 
 export async function searchImages(query) {
     const params = {
@@ -17,20 +15,17 @@ export async function searchImages(query) {
         page: currentPage,
         per_page: perPage,
     };
-};
 
-try {
+    try {
     const response = await axios.get(apiUrl, { params });
     const images = response.data.hits;
     currentPage += 1;
-    return images;
+    return images
 } catch (error) {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-};
+    console.log(error)
+}
+}
 
 export function resetPage() {
     currentPage = 1;
-}
-export function getTotalHits() {
-    return totalHits;
 }
